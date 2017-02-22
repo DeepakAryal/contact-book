1b3754386c8e77146a1fd849511e3b864826e1f6
deepak <deepak@crossovernepal.com>
2017-02-22 16:59:39 +0545

    refactored form type

--- src/ContactBookBundle/Form/ContactFormType.php
+++ src/ContactBookBundle/Form/ContactFormType.php
@@ -7,8 +7,13 @@
  */
 namespace ContactBookBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use ContactBookBundle\Model\Contact;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -28,14 +33,15 @@ class ContactFormType extends AbstractType
     }
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('first_name', 'text', array(
+        $builder->add('first_name', TextType::class, array(
             'label' => 'contact.firstName',
         ));
 
-        $builder->add('last_name', 'text', array(
+        $builder->add('last_name', TextType::class, array(
             'label' => 'contact.lastName',
         ));
-        $builder->add('gender', 'choice', array(
+
+        $builder->add('gender', ChoiceType::class, array(
             'choices' => array(
                 '1' => 'male',
                 '0' => 'female',
@@ -46,34 +52,34 @@ class ContactFormType extends AbstractType
                 'expanded' => true
         ));
 
-        $builder->add('facebook_id', 'text', array(
+        $builder->add('facebook_id', TextType::class, array(
             'label' => 'contact.facebookId',
             'required'    => false,
             'empty_data' => null
 
         ));
 
-        $builder->add('friend_since', 'date', array(
+        $builder->add('friend_since', DateType::class, array(
             'data' => new \DateTime('now'),
             'format' => 'y-M-d',
 
         ));
 
-        $builder->add('email', 'email', array(
+        $builder->add('email', EmailType::class, array(
             'label' => 'contact.email',
             'required' => false,
             'empty_data' => null
 
         ));
-        $builder->add('Addresses', 'collection', array(
+        $builder->add('Addresses', CollectionType::class, array(
            'type' => new AddressFormType(),
 
        ));
-        $builder->add('phones', 'collection', array(
+        $builder->add('phones', CollectionType::class, array(
            'type' => new PhoneFormType(),
 
        ));
-        $builder->add('save', 'submit', array(
+        $builder->add('save', SubmitType::class, array(
 
           'label' => 'contact.save',
        ));
